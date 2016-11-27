fa0ee74f06871abf167f555ed600161065792b4f
Manuel Ramirez <manuelr@pixeleur.com.mx>
2016-11-27 14:12:10 -0800

    feat(api): initial endpoint setup

--- api/app/Http/routes.js
+++ api/app/Http/routes.js
@@ -18,4 +18,24 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
-Route.get('/movies', 'MovieController.list')
+
+// Movies resource
+Route.get('/movies', 'MovieController.list')    // Lists all movies
+
+// Favorites resource
+Route.get('/user/:idUser/favorites', 'UserController.favorites')              // Lists user's favorite movies
+Route.post('/user/:idUser/favorites', 'UserController.favorites')             // creates user's favorite movies
+Route.put('/user/:idUser/favorites/:idFav', 'UserController.favorites')       // updates an user's favorite movie
+Route.delete('/user/:idUser/favorites/:idFav', 'UserController.favorites')    // deletes an user's favorite movie
+
+// Seen resource
+Route.get('/user/:idUser/seen', 'UserController.listSeen')                    // Lists user' seen movies
+Route.post('/user/:idUser/seen', 'UserController.createSeen')                 // creates user' seen movie
+Route.put('/user/:idUser/seen/:idSeen', 'UserController.updateSeen')          // updates an user' seen movie
+Route.delete('/user/:idUser/seen/:idSeen', 'UserController.deleteSeen')       // deletes an user' seen movie
+
+// Watchlist resource
+Route.get('/user/:idUser/watchlist', 'UserController.watchlist')                      // Lists user' watchlist
+Route.post('/user/:idUser/watchlist', 'UserController.createWatchlist')               // creates user' watchlist entry
+Route.put('/user/:idUser/watchlist/:idWlist', 'UserController.updateWatchlist')       // updates an existing user's watchlist entry
+Route.delete('/user/:idUser/watchlist/:idWlist', 'UserController.deleteWatchlist')    // deletes an existing user's watchlist entry
--- api/config/database.js
+++ api/config/database.js
@@ -14,7 +14,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get('DB_CONNECTION', 'mysqll'),
 
   /*
   |--------------------------------------------------------------------------
@@ -49,9 +49,9 @@ module.exports = {
     client: 'mysql',
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      user: Env.get('DB_USER', 'blimflix'),
+      password: Env.get('DB_PASSWORD', 'iT75cRNIOFEVo8KJ'),
+      database: Env.get('DB_DATABASE', 'blimflix')
     }
   },
 
