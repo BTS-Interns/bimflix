8f4023a4de566b2380cb1d2e6afe3a49a19918ef
shockolegario <olegario_90_shock@hotmail.com>
2016-12-05 13:37:53 -0600

    Watchlist resource finished

--- blimflix/client/app/watchlist/watchlist.controller.js
+++ blimflix/client/app/watchlist/watchlist.controller.js
@@ -8,21 +8,7 @@
 
   function WatchlistController(watchlist) {
 
-    var vm = this;
-
-    // watchlist.query({id:1}).$promise.then(function (data) {
-    //   vm.getWatchlist = data;
-    // });
-
-    watchlist.save({id:1, movieID:10, userID:1}).$promise.then(function (data) {
-      vm.postWatchlist = data;
-      //console.log(vm.postWatchlist);
-    });
-
-    // watchlist.remove({id:1, idM:10}).$promise.then(function (data) {
-    //   vm.removeWatchlist = data;
-    //   console.log(data);
-    // });
+    var vm = this;|
 
     vm.activate = activate;
 
