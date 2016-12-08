a3322916b6ac3e31748e07e99fc4a94e32f558e5
shockolegario <olegario_90_shock@hotmail.com>
2016-12-08 13:57:54 -0600

    Seen view integrated for POST method

--- blimflix/client/app/seen/seen.controller.js
+++ blimflix/client/app/seen/seen.controller.js
@@ -1,55 +1,30 @@
 (function() {
   'use strict';
 
-  /**
-   * Chain to fetch module
-   *   https://github.com/johnpapa/angular-styleguide#style-y022
-   */
   angular
     .module('blimFlix.seen')
     .controller('SeenController', SeenController);
     SeenController.$inject = ['seen'];
 
-  /**
-   * Avoid anonymous functions as callbacks
-   *   https://github.com/johnpapa/angular-styleguide#style-y024
-   *
-   * Document dependency injenction using annotations
-   *  https://github.com/johnpapa/angular-styleguide#style-y100
-   */
-  /* @ngInject */
   function SeenController(seen) {
-    // var movies = seen.get({id:1});
-    // console.log(movies);
-    /**
-     * Attach any view properties to this variable
-     *   https://github.com/johnpapa/angular-styleguide#style-y032
-     */
-    /* jshint validthis: true */
-    var vm = this;
 
-    /**
-     * Bindable vm.* members (in alphabetical order)
-     *   All variables here should be part of the vm object and not bound to `this`
-     *   https://github.com/johnpapa/angular-styleguide#style-y033
-     */
+    var vm = this;
+    vm.seenMoviesArray = [];
+    vm.seenMovies = [];
+
+    //Get for seen movies
+    seen.query({ id: 1 }).$promise.then(function (data) {
+      vm.seenMovies = data;
+    });
+
     vm.activate = activate;
 
     activate();
 
-    /**
-     * startup logic goes here
-     *   https://github.com/johnpapa/angular-styleguide#style-y080
-     */
     function activate() {
 
     }
 
-    /**
-     * Non-1-liner view methods here; declare as regular functions
-     *   Implementation details should be declared here and linked via references (note that functions are hoisted up)
-     *   https://github.com/johnpapa/angular-styleguide#style-y034
-     */
   }
 
 })();
