f743285ac794b9f6e9d88cc3c2b4f76a357b87e6
fuksia <sara.fuksia@hotmail.com>
2016-12-09 15:28:23 -0600

    Issues with navigation resource when trying to use data in side-dar directive

--- blimflix/client/app/core/core.controller.js
+++ blimflix/client/app/core/core.controller.js
@@ -8,7 +8,9 @@
   angular
     .module('blimFlix.core')
     .controller('CoreController', CoreController);
-    CoreController.$inject = ['users'];
+
+    CoreController.$inject = ['navigation','users', '$scope'];
+
   /**
    * Avoid anonymous functions as callbacks
    *   https://github.com/johnpapa/angular-styleguide#style-y024
@@ -17,22 +19,28 @@
    *  https://github.com/johnpapa/angular-styleguide#style-y100
    */
   /* @ngInject */
-  function CoreController(users) {
-    
+  function CoreController(navigation, users, $scope) {
+
     /**
      * Attach any view properties to this variable
      *   https://github.com/johnpapa/angular-styleguide#style-y032
      */
     /* jshint validthis: true */
     var vm = this;
+    vm.activate = activate;
+
+    navigation.query({id : 1}).$promise.then(
+      function(data){
+      $scope.navigation = data.navigation;
+      console.log($scope.navigation);
+    });
     /**
      * Bindable vm.* members (in alphabetical order)
      *   All variables here should be part of the vm object and not bound to `this`
      *   https://github.com/johnpapa/angular-styleguide#style-y033
      */
-    vm.activate = activate;
 
-    activate();
+     activate();
 
     /**
      * startup logic goes here
@@ -40,13 +48,12 @@
      */
     function activate() {
 
-    }
-
     /**
      * Non-1-liner view methods here; declare as regular functions
      *   Implementation details should be declared here and linked via references (note that functions are hoisted up)
      *   https://github.com/johnpapa/angular-styleguide#style-y034
      */
   }
+}
 
 })();
--- blimflix/client/app/directives/side-bar/side-bar.directive.js
+++ blimflix/client/app/directives/side-bar/side-bar.directive.js
@@ -10,41 +10,52 @@
     var directive = {
       restrict: 'EA',
       templateUrl: 'app/directives/side-bar/side-bar.directive.html',
-      scope: {},
+      scope: {
+        navigation: "="
+      },
+      replace:true,
       controllerAs: 'vm',
-      controller: SideBarDirectiveController,
-      replace:true
+      link: SideBarDirectiveController,
+
     };
 
     return directive;
   }
 
-  function SideBarDirectiveController($scope) {
-    $scope.sections = [
-      {
-		    index: 0,
-		    title: "All Movies",
-		    count: 30,
-		    Url: "/all"
-   	  },
-      {
-		    index: 1,
-		    title: "Seen",
-		    count: 13,
-		    Url: "/seen"
-   	  },
-      {
-		    index: 2,
-		    title: "Favorites",
-		    count: 8,
-		    Url: "/favorites"
-   	  },
-      {
-		    index: 3,
-		    title: "Watchlist",
-		    count: 19,
-		    Url: "/watchlist"
-   	  }
-    ];
+  function SideBarDirectiveController(scope) {
+
+    scope.$watch('navigation.length',
+      function(oldValue, newValue){
+        console.log(scope.navigation);
+        scope.sections = scope.navigation;
+      }
+    );
+
+
+    //   {
+		//     index: 0,
+		//     title: "All Movies",
+		//     count: 30,
+		//     Url: "/all"
+   // 	  },
+    //   {
+		//     index: 1,
+		//     title: "Seen",
+		//     count: 13,
+		//     Url: "/seen"
+   // 	  },
+    //   {
+		//     index: 2,
+		//     title: "Favorites",
+		//     count: 8,
+		//     Url: "/favorites"
+   // 	  },
+    //   {
+		//     index: 3,
+		//     title: "Watchlist",
+		//     count: 19,
+		//     Url: "/watchlist"
+   // 	  }
+    // ];
   }
 })();
